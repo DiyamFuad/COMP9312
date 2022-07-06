a9a52cfe7f298907e012bb5e9ef8637b1b11a54c
DiyamFuad <105818607+DiyamFuad@users.noreply.github.com>
2022-07-06 13:36:19 +0300

    Update data.py

--- comp9312/ner/data.py
+++ comp9312/ner/data.py
@@ -43,7 +43,7 @@ class BertSeqTransform:
             print("self.vocab.tags[token.gold_tag]", self.vocab.tags[token.gold_tag])
             token_subwords = self.encoder(token.text)
             subwords += token_subwords
-            tags += [self.vocab.tags[token.gold_tag]] + [self.vocab.tags["O"]] * (len(token_subwords) - 1)
+            tags += [self.vocab.tags[token.gold_tag]] + [self.vocab.tags["اسم"]] * (len(token_subwords) - 1)
             tokens += [token] + [unk_token] * (len(token_subwords) - 1)
 
         # Truncate to max_seq_len
@@ -57,8 +57,8 @@ class BertSeqTransform:
         subwords.insert(0, self.tokenizer.cls_token_id)
         subwords.append(self.tokenizer.sep_token_id)
 
-        tags.insert(0, self.vocab.tags["O"])
-        tags.append(self.vocab.tags["O"])
+        tags.insert(0, self.vocab.tags["اسم"])
+        tags.append(self.vocab.tags["اسم"])
 
         tokens.insert(0, unk_token)
         tokens.append(unk_token)
